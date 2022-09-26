52f76fd0a546ad864bc8e3d5a871dcb7bfd55b20
dheniffe <dheniffe.oliveira@gmail.com>
2022-09-25 23:29:29 -0300

    header mobile

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import MenuIcon from '../../assets/icons/menuIcon.png';
-import * as S from './Style.js'
+import React from "react";
+//import LogoDesktop from "../../assets/icons/RC.svg";
+import MenuIcon from "../../assets/icons/menuIcon.svg";
+import * as S from "./Style.js";
 
 export default function Header() {
   return (
     <S.Header>
-      <S.BoxMenuHamburger>
-        <S.MenuHamburger src={MenuIcon} alt='Menu hamburger'/>
-      </S.BoxMenuHamburger>
+      <nav>
+        <S.BoxMenuHamburger>
+          <S.MenuHamburger src={MenuIcon} alt="Menu hamburger" />
+          <S.MenuMobileContent>
+            <S.BoxMobileList>
+              <S.MenuMobileItem>ABOUT</S.MenuMobileItem>
+              <S.MenuMobileItem>RECIPES</S.MenuMobileItem>
+              <li>
+                <S.SubscribesButton>SUBSCRIBES</S.SubscribesButton>
+              </li>
+            </S.BoxMobileList>
+          </S.MenuMobileContent>
+        </S.BoxMenuHamburger>
+      </nav>
       <S.BoxTitle>
         <S.Title>RECIPES</S.Title>
       </S.BoxTitle>
--- src/components/header/Style.js
+++ src/components/header/Style.js
@@ -10,17 +10,87 @@ export const Header = styled.header`
   background-size: cover;
   background-position: 80%;
   background-repeat: no-repeat;
-  font-family: 'HURME GEOMETRIC';
+  font-family: "HURME GEOMETRIC";
+`;
+
+// export const MenuDesktop = styled.section `
+//   padding: 2.8rem 3.3rem 0 3.9rem;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+// `
+
+// export const MenuDesktopContent = styled.div`
+//   border: 1px solid #000;
+// `
+
+// export const BoxDesktopList = styled.ul `
+//   border: 1px solid red;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `
+
+// export const MenuDesktopItem = styled.li`
+//   border: 1px solid green;
+//   margin-left: 5.5rem;
+//   letter-spacing: .1rem;
+// `
+export const SubscribesButton = styled.button`
+  padding: .5rem 3rem;
+  font-family: "HURME GEOMETRIC";
+  font-size: 1.2rem;
+  color: #373737;
+  border: 5px solid #373737;
+  background-color: transparent;
 `;
 
 export const BoxMenuHamburger = styled.figure`
-  text-align: end;
+  width: 100%;
+  display: flex;
+  justify-content: end;
+
+  @media(min-width: 1000px) {
+    display: none;
+  }
 `;
 
 export const MenuHamburger = styled.img`
+  position: relative;
   width: 2rem;
   margin: 1.4rem 0.5rem 0 0;
 `;
+
+export const MenuMobileContent = styled.div`
+  display: none; /* só fica flex quando clicar no menu hamburguer */
+  position: absolute;
+  padding: 2rem;
+  width: calc(100vw - 16px);
+  background-color: #dfe4de;
+`;
+
+export const BoxMobileList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 1.5rem;
+  font-weight: 700;
+`;
+
+export const MenuMobileItem = styled.li`
+  margin-bottom: 2rem;
+`;
+
+/*export const SubscribesButton = styled.button`
+  padding: 0.7rem;
+  font-family: "HURME GEOMETRIC";
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: #373737;
+  border-radius: 6px;
+  background-color: #FFFFF;
+`;*/
+
 
 export const BoxTitle = styled.div`
   display: flex;
